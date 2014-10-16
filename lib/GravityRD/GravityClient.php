d47448ea71423bf838262d638610487e901dcb4d
Bozsányi Ádám <hunslater@gmail.com>
2014-10-16 10:28:43 +0200

    version 1.0.2

--- lib/GravityRD/GravityClient.php
+++ lib/GravityRD/GravityClient.php
@@ -39,7 +39,7 @@ class GravityClient {
 	/**
 	 * The version info of the client.
 	 */
-	private $version = '1.0.1';
+	private $version = '1.0.2';
 
 	/**
 	 * Creates a new client instance with the specified configuration
@@ -321,9 +321,9 @@ class GravityEvent {
 	 * The event type determines the namevalues which can be passed.
 	 * <p>Possible list event types, which can be expanded based on what can the external system support:</p>
 	 * <table border="1">
-	 *	<tr><th><code>Event Type</code></th><th>Description</th><th>NameValues for the event</th></tr>
-	 *	<tr><td><code>VIEW</code></td><td>The user viewed the info page of an item.</td><td></td></tr>
-	 *	<tr><td><code>BUY</code></td><td>The user bought an item.</td><td>
+	 *	<tr><th><code>Event Type</code><th>Category</th></th><th>Description</th><th>NameValues for the event</th></tr>
+	 *	<tr><td><code>VIEW</code></td><td>GENERAL</td><td>The user viewed the info page of an item.</td><td></td></tr>
+	 *	<tr><td><code>BUY</code></td><td>GENERAL</td><td>The user bought an item.</td><td>
 	 *		<table>
 	 *			<tr><td><code>OrderId</code></td><td></td></tr>
 	 *			<tr><td><code>UnitPrice</code></td><td>Formatted as a decimal number, for example 1234 or 12345.67</td></tr>
@@ -331,58 +331,67 @@ class GravityEvent {
 	 *			<tr><td><code>Quantity</code></td><td>Formatted as a decimal number.</td></tr>
 	 *		</table>
 	 *	</td></tr>
-	 *	<tr><td><code>RATING</code></td><td>The user rated an item.</td><td>
+	 *	<tr><td><code>RATING</code></td><td>GENERAL</td><td>The user rated an item.</td><td>
 	 *		<table>
 	 *			<tr><td><code>Value</code></td><td>The value of the rating.</td></tr>
 	 *		</table>
 	 *	</td></tr>
-	 *	<tr><td><code>ADD_TO_CART</code></td><td>The user added an item to the shopping cart.</td><td>
+	 *	<tr><td><code>ADD_TO_CART</code></td><td>GENERAL</td><td>The user added an item to the shopping cart.</td><td>
 	 *		<table>
 	 *			<tr><td><code>Quantity</code></td><td></td></tr>
 	 *		</table>
 	 *	</td></tr>
-	 *	<tr><td><code>ADD_TO_WISHLIST</code></td><td>The user added the item to his wishlist.</td><td>
-	 *		<table>
-	 *			<tr><td><code>ListId</code></td><td>Use if the webshop supports multiple wishlists.</td></tr>
-	 *		</table>
-	 *	</td></tr>
-	 *	<tr><td><code>ADD_TO_FAVORITES</code></td><td>The user added the item to his favorites.</td><td>
-	 *		<table>
-	 *			<tr><td><code>ListId</code></td><td>Use if the webshop supports multiple favorites lists.</td></tr>
-	 *		</table>
-	 *	</td></tr>
-	 *	<tr><td><code>REMOVE_FROM_CART</code></td><td>The user removed an item from the shopping cart.</td><td>
+	 *	<tr><td><code>REMOVE_FROM_CART</code></td><td>GENERAL</td><td>The user removed an item from the shopping cart.</td><td>
 	 *		<table>
 	 *			<tr><td><code>Quantity</code></td><td></td></tr>
 	 *		</table>
 	 *	</td></tr>
-	 *	<tr><td><code>REMOVE_FROM_WISHLIST</code></td><td>The user removed an item from his wishlist.</td><td>
-	 *		<table>
-	 *			<tr><td><code>ListId</code></td><td>Use if the webshop supports multiple wishlists.</td></tr>
-	 *		</table>
-	 *	</td></tr>
-	 *	<tr><td><code>REMOVE_FROM_FAVORITES</code></td><td>The user removed an item from his favorites.</td><td>
+	 *	<tr><td><code>ADD_TO_FAVORITES</code></td><td>GENERAL</td><td>The user added the item to his favorites.</td><td>
 	 *		<table>
 	 *			<tr><td><code>ListId</code></td><td>Use if the webshop supports multiple favorites lists.</td></tr>
 	 *		</table>
 	 *	</td></tr>
-	 *	<tr><td><code>REC_CLICK</code></td><td>The user clicked on a recommended item.</td><td>
+	 *	<tr><td><code>REMOVE_FROM_FAVORITES</code></td><td>GENERAL</td><td>The user removed an item from his favorites.</td><td>
+	 *		<table>
+	 *			<tr><td><code>ListId</code></td><td>Use if the webshop supports multiple favorites lists.</td></tr>
+	 *		</table>
+	 *	</td></tr>
+	 * 	<tr><td><code>REC_CLICK</code></td><td>GENERAL</td><td>The user clicked on a recommended item.</td><td>
 	 *		<table>
 	 *			<tr><td><code>Position</code></td><td>The position of the clicked item in the recommendation list. The position of the first item is 1.</td></tr>
 	 *		</table>
 	 *	</td></tr>
-	 *	<tr><td><code>HIDE_PRODUCT</code></td><td>The user hides a product that should not be recommended to him.</td><td></td></tr>
-	 *	<tr><td><code>UNHIDE_PRODUCT</code></td><td>The user unhides a product that he marked as hidden previously.</td><td></td></tr>
-	 *	<tr><td><code>PRODUCT_SEARCH</code></td><td>A list of products was displayed to the user, for example by browsing a category or by free text search.</td><td>
+	 *	<tr><td><code>LOGIN</code></td><td>GENERAL</td><td>The user logged in to the site. For this event the cookieId and also the userId must be specified.</td><td></td></tr>
+	 *	<tr><td><code>ADD_TO_WISHLIST</code></td><td>ADDITIONAL</td><td>The user added the item to his wishlist.</td><td>
+	 *		<table>
+	 *			<tr><td><code>ListId</code></td><td>Use if the webshop supports multiple wishlists.</td></tr>
+	 *		</table>
+	 *	</td></tr>
+	 *	<tr><td><code>REMOVE_FROM_WISHLIST</code></td><td>ADDITIONAL</td><td>The user removed an item from his wishlist.</td><td>
+	 *		<table>
+	 *			<tr><td><code>ListId</code></td><td>Use if the webshop supports multiple wishlists.</td></tr>
+	 *		</table>
+	 *	</td></tr>
+	 *	<tr><td><code>HIDE_PRODUCT</code></td><td>ADDITIONAL</td><td>The user hides a product that should not be recommended to him.</td><td></td></tr>
+	 *	<tr><td><code>UNHIDE_PRODUCT</code></td><td>ADDITIONAL</td><td>The user unhides a product that he marked as hidden previously.</td><td></td></tr>
+	 *	<tr><td><code>PRODUCT_SEARCH</code></td><td>ADDITIONAL</td><td>A list of products was displayed to the user, for example by browsing a category or by free text search.</td><td>
 	 *		<table>
 	 *			<tr><td><code>SearchString</code></td><td>The search string, if the list is based on a free text search.</td></tr>
 	 *			<tr><td><code>Filter.*</code></td><td>If the listing is based on comparing an item namevalue to a filter value, you can provide the actual filter here.
 	 *				For example, if the user was browsing a specific category, name='Filter.CategoryId' and value='CategoryA' can be specified.</td></tr>
 	 *		</table>
 	 *	</td></tr>
-	 *	<tr><td><code>LOGIN</code></td><td>The user logged in to the site. For this event the cookieId and also the userId must be specified.</td><td></td></tr>
-	 *	<tr><td><code>NEXT_RECOMMENDATION</code></td><td>The user asked for more recommendation.</td><td></td></tr>
-	 *	<tr><td><code>CLICK_OUT</code></td><td>The user jumps to an external webshop to buy the product. Used by price comparison sites.</td><td></td></tr>
+	 *	<tr><td><code>NEXT_RECOMMENDATION</code></td><td>ADDITIONAL</td><td>The user asked for more recommendation.</td><td></td></tr>
+	 *	<tr><td><code>CLICK_OUT</code></td><td>PRICE COMPARISON</td><td>The user jumps to an external webshop to buy the product. Used by price comparison sites.</td><td></td></tr>
+	 *	<tr><td><code>LANCE</code></td><td>AUCTION</td><td>The user place a bid on the item.</td><td><code>Value</code>The value of the bid as a decimal number.</td></tr>
+	 *	<tr><td><code>FREE_VIEW</code></td><td>MEDIA</td><td>The user wached/listened an item for free.</td><td><code>Duration</code>How long the user wached the item in seconds as a decimal number.</td></tr>
+	 *	<tr><td><code>PAID_VIEW</code></td><td>MEDIA</td><td>The user payed for waching/listening an item.</td><td>
+	 *      <table>
+	 *			<tr><td><code>Duration</code></td><td>How long the user wached the item in seconds. A decimal number.</td></tr>
+	 *			<tr><td><code>Value</code></td><td>How much the user payed for waching the item. A decimal number.</td></tr>
+	 *		</table>
+	 *  </td></tr>
+	 *	<tr><td><code>LETTER_SEND</code></td><td>ADVERTISING</td><td>The user sent a message to the advertiser.</td><td></td></tr>
 	 * </table>
 	 *
 	 * @var string
